a390afd088e7dae826bc7fa05a6526d265723fcf
Sid <Sid12869@yahoo.co.uk>
2019-05-19 15:05:33 +0100

    intial commit

--- DriverApplication/Global.asax.cs
+++ DriverApplication/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using DriverApplication.App_Start;
 
 namespace DriverApplication
 {
@@ -13,6 +14,7 @@ namespace DriverApplication
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
